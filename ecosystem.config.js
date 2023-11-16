5851e2cb396fce473ba447533eaf719b969fc66e
juri <j.koniwszczynski@gmail.com>
2023-11-16 16:15:50 +0100

    Clean code for new deploy on prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,23 +11,4 @@ module.exports = {
       NODE_ENV: "prod"
     }
   }],
-  deploy: {
-    prod: {
-      key: "./deploy.key",
-      ssh_options: "StrictHostKeyChecking=no",
-      user: "ec2-user",
-      host: [
-        {
-          host: "3.20.183.221",
-          port: "4578",
-        },
-      ],
-      ref: "main",
-      repo: "git@github.com:SkyTradeLinks/address-claiming.git",
-      path: "/home/ec2-user/pm2-deploy/address-claiming",
-      "post-deploy":
-      "bash aws_ps_get_prod_env.sh && yarn && yarn build && pm2 startOrReload ecosystem.config.js --env prod",
-      "pre-setup": "ssh-keyscan github.com >> ~/.ssh/known_hosts",
-    },
-  },
 }
