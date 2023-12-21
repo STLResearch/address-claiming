d58841273f491eb7ce69cd109a9eb831e75e2187
mz7 <3603681+mz7mz7mz7@users.noreply.github.com>
2023-12-21 14:16:50 +0100

    Revert "add cookie consent"

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '@/store/store';
 import Script from 'next/script';
-import CookieConsent from '@/Components/CookieConsent'
 
 import { AuthProvider } from '@/hooks/useAuth';
 
@@ -13,7 +12,6 @@ export default function App({ Component, pageProps }) {
         <>
           <Script src='https://cdn.withpersona.com/dist/persona-v4.8.0.js' />
           <Component {...pageProps} />
-          <CookieConsent /> 
         </>
       </Provider>
     </AuthProvider>
