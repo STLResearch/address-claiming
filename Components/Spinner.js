80f2d4b59f2c7d3bff292e74c183e3d6141f8867
Blessing600 <julietogbodo11@gmail.com>
2024-03-07 14:50:10 +0100

    added functionlity on claim airspace by drawing polygon on the map

--- Components/Spinner.js
+++ Components/Spinner.js
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
-    <div className='flex flex-col items-center justify-center z-50'>
-      <span className='loader'></span>
+    <div className="flex flex-col items-center justify-center z-50">
+      <span className="loader"></span>
     </div>
   );
 };
