36237f6c53eeca14f1d373cf228bebd3bcc06c84
emnet yohannes <emnet.yohan@gmail.com>
2024-03-20 10:47:53 +0300

    removed unused shares

--- Components/ShareButtons/ShareButtons.jsx
+++ Components/ShareButtons/ShareButtons.jsx
@@ -3,16 +3,13 @@ import {
   FacebookShareButton,
   TwitterShareButton,
   LinkedinShareButton,
-  FacebookMessengerShareButton,
   TelegramShareButton,
   WhatsappShareButton,
   RedditShareButton
 } from "react-share";
 
-import { FacebookIcon, XIcon, LinkedinIcon,FacebookMessengerIcon,TelegramIcon,WhatsappIcon,RedditIcon } from "react-share";
-import useOrigin from "@/hooks/useOrigin";
-const ShareButtons = ({ url, title, text, hashtags ,setIsSharing}) => {
-  const origin = useOrigin();
+import { FacebookIcon, XIcon, LinkedinIcon,TelegramIcon,WhatsappIcon,RedditIcon } from "react-share";
+const ShareButtons = ({ url,  text ,setIsSharing}) => {
   return (
     <div className="flex w-full justify-center space-x-10 ">
       <FacebookShareButton  quote={text} url={url}>
@@ -30,11 +27,6 @@ const ShareButtons = ({ url, title, text, hashtags ,setIsSharing}) => {
           <LinkedinIcon size={24} round />
         </div>
       </LinkedinShareButton>
-      {/* <FacebookMessengerShareButton url={url} source={url}>
-        <div className=" flex items-center justify-center">
-          <FacebookMessengerIcon size={24} round />
-        </div>
-      </FacebookMessengerShareButton> */}
       <TelegramShareButton url={url}>
         <div className=" flex items-center justify-center">
           <TelegramIcon size={24} round />
