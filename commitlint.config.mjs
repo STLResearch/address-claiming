14467a8d9fabc02d71db639f5253f6907318cc9b
Saty248 <mondal.sayantan1234@gmail.com>
2024-04-17 17:11:05 +0530

    feat: added husky and commit lint ,ref:#335

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,5 @@ export default {
   extends: [
     '@commitlint/config-conventional' // scoped packages are not prefixed
   ],
-  rules: { 'references-empty': [2, 'always'] }
+  rules: { 'references-empty': [2, 'never'] }
 };
