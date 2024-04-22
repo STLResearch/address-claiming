0b153230b0b63904b233ecf8b9abc07bedda28b8
emnet yohannes <emnet.yohan@gmail.com>
2024-04-22 10:23:40 +0300

    added on a review and rejected for the time

--- pages/homepage/account/index.js
+++ pages/homepage/account/index.js
@@ -184,6 +184,20 @@ const Portfolio = () => {
               },
             });
         };
+        const getStatusMessage = (statusId) => {
+            switch (statusId) {
+              case 0:
+                return "Your account is not verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.";
+              case 1:
+                return "Our team is currently conducting a review of your Know Your Customer (KYC) documentation. We appreciate your patience during this process and will notify you promptly upon its completion.";
+              case 2:
+                return "Thank you, your account is successfully verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.";
+              case 3:
+                return "We regret to inform you that your account verification was unsuccessful.";
+              default:
+                return "";
+            }
+          };
     return (
         <Fragment>
             {isLoading && createPortal(<Backdrop />, document?.getElementById('backdrop-root'))}
@@ -201,11 +215,14 @@ const Portfolio = () => {
                         <div className="flex flex-col py-[17px] px-[25px] rounded-[30px] gap-[15px] bg-white" style={{ boxShadow: '0px 12px 34px -10px #3A4DE926' }}>
                             <div className="flex items-center justify-between">
                                 <h2 className="text-xl font-medium text-[#222222]">Account verification</h2>
-                                {personalInformation.KYCStatusId !== 0 && <div className="flex justify-center items-center bg-[#1FD387] rounded-[50%] w-[42px] h-[42px]">
+                                {personalInformation.KYCStatusId === 2 && <div className="flex justify-center items-center bg-[#1FD387] rounded-[50%] w-[42px] h-[42px]">
                                     <div className="w-[14px] h-[17.73px]"><ShieldIcon /></div>
                                 </div>}
                             </div>
-                            <p className="font-normal text-base text-[#87878D] pr-[42px]">{personalInformation.KYCStatusId !== 0 ? 'Thank you, your account is successfully verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.' : 'Your account is not verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.'}</p>
+                            {/* <p className="font-normal text-base text-[#87878D] pr-[42px]">{personalInformation.KYCStatusId !== 0 ? 'Thank you, your account is successfully verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.' : 'Your account is not verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Note that we store your data securely with advanced encryption and strict authentication measures to ensure utmost privacy and protection.'}</p> */}
+                            <p className="font-normal text-base text-[#87878D] pr-[42px]">
+                                {getStatusMessage(personalInformation.KYCStatusId)}
+                            </p>
                             {!(personalInformation.KYCStatusId !== 0) &&!isLoading && <p className="font-medium text-base text-[#0653EA] text-right flex-1 cursor-pointer" onClick={onVerifyMyAccount}>Verify my account</p>}
                         </div>
                         <div className="flex flex-col py-[17px] px-[25px] rounded-[30px] gap-[15px] bg-white" style={{ boxShadow: '0px 12px 34px -10px #3A4DE926' }}>
