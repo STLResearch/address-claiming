1c87e1a128715dc12219e22c219879e3950f92c8
Saty248 <mondal.sayantan1234@gmail.com>
2024-05-17 03:26:45 +0530

    update: added login component to private pages for public  users

---
+++ Components/common/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Signup from '@/pages/auth/join'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+
+
+    <div className='fixed top-[10%] left-[35%] w-[25%] h-[85%]  rounded-xl z-40 overflow-y-scroll'>
+        <Signup/>
+    </div>
+  )
+}
