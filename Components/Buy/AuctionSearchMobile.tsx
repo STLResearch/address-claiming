03c007568cfe4ef1f1b2c1e2696eca26b4847e91
emnet yohannes <emnet.yohan@gmail.com>
2024-07-15 14:04:57 +0300

    search debounce for mobile

--- Components/Buy/AuctionSearchMobile.tsx
+++ Components/Buy/AuctionSearchMobile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { IoArrowBack } from "react-icons/io5";
 import { MagnifyingGlassIcon } from "../Shared/Icons";
 import { GiSettingsKnobs } from "react-icons/gi";
@@ -10,6 +10,7 @@ import {
   setActiveFilters,
   setIsCreateAuctionModalOpen,
 } from "@/redux/slices/userSlice";
+import debounce from 'debounce';
 import { shallowEqual } from "react-redux";
 
 interface AuctionSearchMobileProps {
@@ -47,10 +48,24 @@ const AuctionSearchMobile: React.FC<AuctionSearchMobileProps> = ({
     const activeFilters = calculateActiveFilters();
     dispatch(setActiveFilters(activeFilters));
   };
-  const handleSearchAuctions = async () => {
+
+  const handleSearchAuctions = (searchValue) => {
     setSearchTerm(searchValue);
   };
 
+  const debounceLoadData = useMemo(() => debounce(handleSearchAuctions,3000), []);
+
+  useEffect(() => {
+    const searchElement = document.getElementById('search_auctions');
+    
+    if (searchElement) {
+      const handleInput = (event) => debounceLoadData(event.target.value);
+      searchElement.addEventListener('input', handleInput);
+      return () => {
+        searchElement.removeEventListener('input', handleInput);
+      };
+    }
+  }, [debounceLoadData]);
   return (
     <div className="md:hidden fixed top-0 left-0 w-full z-20 bg-white p-4 shadow-md text-center">
       <div className="flex items-center justify-between gap-2">
@@ -59,14 +74,14 @@ const AuctionSearchMobile: React.FC<AuctionSearchMobileProps> = ({
         </div>
         <div className="flex justify-between items-center border rounded-lg overflow-hidden p-2">
           <input
+            id="search_auctions"
             placeholder="Search auctions..."
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
-            onKeyDown={(e) => (e.key === "Enter" ? handleSearchAuctions() : "")}
             className="focus:outline-none min-w-[200px] text-[14px]"
           />
 
-          <div className="w-4 h-4" onClick={handleSearchAuctions}>
+          <div className="w-4 h-4" >
             <MagnifyingGlassIcon />
           </div>
         </div>
