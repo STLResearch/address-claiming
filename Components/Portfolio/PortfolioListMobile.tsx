c76615b9592f1ed48ee0647ce5e328a85749a923
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-09-17 18:19:41 +0100

    Fix: Resolve issue with airspace details not displaying on mobile devices for Verified Airspaces and other tabs

--- Components/Portfolio/PortfolioListMobile.tsx
+++ Components/Portfolio/PortfolioListMobile.tsx
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import Spinner from "../Spinner";
 import PortfolioItemMobile from "./PortfolioItemMobile";
 import AirspacesEmptyMessage from "./AirspacesEmptyMessage";
 import usePortfolioList, { PortfolioTabEnum } from "@/hooks/usePortfolioList";
-import { PropertyData } from "@/types";
+import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import Modal from "../Portfolio/Modal";
+import { PropertyData, StatusTypes } from "@/types";
 
 interface PropsI {
   selectAirspace: (data: PropertyData) => void;
+  selectedAirspace: PropertyData | null;
+  onCloseModal: () => void;
   setUploadedDoc: any;
   uploadedDoc: any;
 }
 
+const PortfolioListMobile = ({ selectAirspace, selectedAirspace, onCloseModal, setUploadedDoc, uploadedDoc }: PropsI) => {
 
-const PortfolioListMobile = ({ selectAirspace, setUploadedDoc, uploadedDoc }: PropsI) => {
-  const {
-    handleTabSwitch,
-    handlePrevPage,
-    handleNextPage,
-    loading,
-    airspaceList,
-    pageNumber,
-    activeTab
-  } = usePortfolioList();
+    const { user } = useAuth();
+    const router = useRouter();
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+  
+    const {
+      handleTabSwitch,
+      handlePrevPage,
+      handleNextPage,
+      loading,
+      pageNumber,
+      airspaceList,
+      activeTab,
+      setAirspaceList
+    } = usePortfolioList()
+  
+    useEffect(() => {
+      if (user && user.KYCStatusId === StatusTypes.NotAttempted) {
+        setShowPopup(true);
+        return
+      }
+    }, [user?.KYCStatusId])
+  
   return (
+    <>
+    {selectedAirspace !== null && (
+      <Modal airspace={selectedAirspace} onCloseModal={onCloseModal} setAirspaceList={setAirspaceList} />
+    )}
     <div className="overflow-x-hidden mb-24">
       <div
         className="flex items-center overflow-x-scroll border-b border-[#5D7285]/50 gap-12"
@@ -74,6 +96,25 @@ const PortfolioListMobile = ({ selectAirspace, setUploadedDoc, uploadedDoc }: Pr
       ) : (
         <div className="w-screen ">
           <div className="flex flex-col gap-[2px] pb-2  min-h-[70vh] ">
+          {activeTab === PortfolioTabEnum.UNVERIFIED && showPopup &&
+                <div className="flex w-full rounded-[30px] gap-[15px] bg-white" style={{ boxShadow: "0px 12px 34px -10px #3A4DE926" }} >
+                  <div className="md:w-[50%]  p-6  flex flex-col justify-center items-center md:gap-6 gap-4">
+                    <h1 className="text-xl font-medium text-[#222222]  text-center">🚀 Attention Airspace Owner!</h1>
+                    <h1 className="text-xl font-medium text-[#222222] block md:hidden">Account verification</h1>
+                    <p className="text-sm font-normal text-[#838187] text-center leading-6">Your airspace awaits verification by our operation team. Your account is not verified. We verify the identity of our customers to assess potential risks, prevent fraud, and comply with legal and regulatory requirements. Complete your KYC to expedite the process and ensure swift approval. Plus,<span className="text-[#87878D] text-sm font-bold" > earn 10 SKY points </span> as a token of our appreciation! Don't delay - verify now and unlock the full potential of your airspace!</p>
+
+                    <button onClick={() => router.push('/my-account')} className="text-sm font-medium w-full px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">Verify my identity Now</button>
+
+                  </div>
+                  <div className="w-[50%]">
+                    <img
+                      src="/images/portfolio.png"
+                      alt="Verification Image"
+                      className="h-full w-full object-cover rounded-r-[30px]"
+                    />
+                  </div>
+                </div>
+              }
             {airspaceList &&
               airspaceList[0] &&
               airspaceList[0].address ? (
@@ -116,6 +157,7 @@ const PortfolioListMobile = ({ selectAirspace, setUploadedDoc, uploadedDoc }: Pr
         </div>
       )}
     </div>
+    </>
   );
 };
 export default PortfolioListMobile;
