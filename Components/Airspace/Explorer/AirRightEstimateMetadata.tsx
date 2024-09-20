74129e409607ea0208f1a5481b7f6bbf8be2584b
Victor Olowe <vikie95@gmail.com>
2024-09-20 15:03:51 +0100

    [#1146] Airspace: Add estimated values of properties

---
+++ Components/Airspace/Explorer/AirRightEstimateMetadata.tsx
@@ -0,0 +1,29 @@
+interface IAirRightEstimateMetadataProps {
+  title: string;
+  value: string;
+  icon: React.ReactNode;
+}
+
+const labelClassNames = "text-[#0E2B56] font-poppins";
+
+function AirRightEstimateMetadata(props: IAirRightEstimateMetadataProps) {
+  return (
+    <div className="flex items-center rounded-xl bg-[#DEE9F8] px-5 py-1">
+      <div className="w-5 h-5">{props.icon}</div>
+
+      <div className="flex flex-col ml-6">
+        <span className={`${labelClassNames} text-[13px]`}>{props.title}</span>
+
+        <span className={`${labelClassNames} font-bold`}>
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            maximumFractionDigits: 0,
+          }).format(Number(props.value))}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default AirRightEstimateMetadata;
