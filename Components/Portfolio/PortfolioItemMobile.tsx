8a1ebeaa2d1962c55cd2b948749ae65f9d2ae697
Ikenna U <iykethe1st@gmail.com>
2024-10-29 20:53:30 +0100

    fix: hot fix

--- Components/Portfolio/PortfolioItemMobile.tsx
+++ Components/Portfolio/PortfolioItemMobile.tsx
@@ -16,7 +16,6 @@ import Modal from "./Modal";
 import CancelClaimModal from "./CancelClaimModal";
 
 interface PropsI {
-  airspaceName: string;
   activeTab: PortfolioTabEnum;
   tags: Boolean[];
   type: string | undefined;
@@ -27,9 +26,9 @@ interface PropsI {
   onCloseModal: () => void;
   setAirspaceList: React.Dispatch<React.SetStateAction<PropertyData[]>>;
   selectedAirspace: any;
-  requestDocument: RequestDocument[] | undefined;
   setSelectedAirspace: any;
   createdAt: Date;
+  airspace?: any;
 }
 function formatDate(isoDateStr) {
   const date = new Date(isoDateStr);
@@ -42,7 +41,6 @@ function formatDate(isoDateStr) {
 
 const PortfolioItemMobile = ({
   refetchAirspaceRef,
-  airspaceName,
   tags,
   type,
   selectAirspace,
@@ -52,9 +50,9 @@ const PortfolioItemMobile = ({
   onCloseModal,
   setAirspaceList,
   selectedAirspace,
-  requestDocument,
   setSelectedAirspace,
   createdAt,
+  airspace,
 }: PropsI) => {
   const [showPopup, setShowPopup] = useState(false);
   const [showSuccessToast, setShowSuccessToast] = useState(false);
@@ -80,12 +78,20 @@ const PortfolioItemMobile = ({
     modalRef.current = false;
     setShowModal(true);
   };
-  const documentStatus = checkDocumentStatus(requestDocument);
+  const documentStatus = checkDocumentStatus(airspace?.requestDocument);
   const [showCancelModal, setShowCancelModal] = useState(false);
 
   return (
     <div>
-      {showModal && <Modal airspace={selectedAirspace} onCloseModal={() => {onCloseModal(); setShowModal(false)}} />}
+      {showModal && (
+        <Modal
+          airspace={selectedAirspace}
+          onCloseModal={() => {
+            onCloseModal();
+            setShowModal(false);
+          }}
+        />
+      )}
       {showCancelModal && (
         <CancelClaimModal
           airspace={selectedAirspace}
@@ -94,124 +100,128 @@ const PortfolioItemMobile = ({
           setAirspaceList={setAirspaceList}
         />
       )}
-      <div>
-        <div className="w-screen cursor-pointer items-center justify-between gap-[10px] rounded-lg bg-white px-4 py-6 shadow-md">
-          <div className="items-center justify-between gap-[10px] rounded-lg">
-            <div className="flex flex-1 items-center gap-[10px]">
-              <div className="h-6 w-6">
-                <LocationPointIcon />
-              </div>
-              <p className="flex-1 text-[14px] font-normal text-[#222222]">
-                {airspaceName.length > 15 ? airspaceName.slice(0, 25) + " ..." : airspaceName}
-              </p>
-            </div>
-            <div className="">
-              <div className="mt-2 flex w-full items-center justify-between gap-[10px]">
-                {!!tags[0] && (
-                  <div
-                    onClick={handleOnClaim}
-                    className="flex h-[27px] cursor-pointer items-center justify-center rounded-[3px] bg-[#DBDBDB] p-2 text-sm font-normal text-[#222222]"
-                  >
-                    {type === "land" ? `Claim Date: ${formatDate(createdAt)}` : "On Rent"}
-                  </div>
-                )}
-                {!!tags[1] && (
-                  <div className="cursor-pointer rounded-[3px] bg-[#E7E6E6] px-[7px] text-sm font-normal text-[#222222]">
-                    On Sale
-                  </div>
-                )}
-                {!!tags[2] && (
-                  <div className="cursor-pointer rounded-[3px] bg-[#222222] px-[7px] text-sm font-normal text-white">
-                    No Fly Zone
-                  </div>
-                )}
-                {!!tags[3] && (
-                  <div className="cursor-pointer rounded-[3px] bg-[#E04F64] px-[7px] text-sm font-normal text-white">
-                    Review Offer
-                  </div>
-                )}
-                {activeTab === PortfolioTabEnum.UNVERIFIED && (
-                  <div
-                    onClick={handleCancelClaim}
-                    className="flex h-8 w-28 cursor-pointer items-center justify-center rounded-[3px] bg-[#4285F4] px-2 text-sm font-normal text-white"
-                  >
-                    Cancel Claim
-                  </div>
-                )}
 
-                {(documentStatus === "SUBMITTED" || underReview) && (
-                  <div className="flex items-center justify-center gap-2">
-                    <div className="h-6 w-6">
-                      <ReviewVerificationIcon />
+      {airspace.type === "receivedBid" || airspace.type === "placedBid" ?
+        <></>
+      : <div>
+          <div className="w-screen cursor-pointer items-center justify-between gap-[10px] rounded-lg bg-white px-4 py-6 shadow-md">
+            <div className="items-center justify-between gap-[10px] rounded-lg">
+              <div className="flex flex-1 items-center gap-[10px]">
+                <div className="h-6 w-6">
+                  <LocationPointIcon />
+                </div>
+                <p className="flex-1 text-[14px] font-normal text-[#222222]">
+                  {airspace.address.length > 15 ? airspace.address.slice(0, 25) + " ..." : airspace.address}
+                </p>
+              </div>
+              <div className="">
+                <div className="mt-2 flex w-full items-center justify-between gap-[10px]">
+                  {!!tags[0] && (
+                    <div
+                      onClick={handleOnClaim}
+                      className="flex h-[27px] cursor-pointer items-center justify-center rounded-[3px] bg-[#DBDBDB] p-2 text-sm font-normal text-[#222222]"
+                    >
+                      {type === "land" ? `Claim Date: ${formatDate(createdAt)}` : "On Rent"}
                     </div>
-                    <p className="text-sm font-normal text-orange-500">Documents under review</p>
-                  </div>
-                )}
-                {documentStatus === "APPROVED" && !underReview && (
-                  <div className="flex items-center justify-center gap-2">
-                    <div className="h-6 w-6">
-                      <DocumentApprovedIcon />
+                  )}
+                  {!!tags[1] && (
+                    <div className="cursor-pointer rounded-[3px] bg-[#E7E6E6] px-[7px] text-sm font-normal text-[#222222]">
+                      On Sale
                     </div>
-                    <p className="text-sm font-normal text-[#1FD387]">Documents approved</p>
-                  </div>
-                )}
-                {(documentStatus === "REJECTED" || documentStatus === "RE_UPLOAD") && !underReview && (
-                  <div className="">
-                    <div className="flex items-center justify-end">
-                      <div className="mr-[10px] h-4 w-4">
-                        <DocumentRejectedIcon />
+                  )}
+                  {!!tags[2] && (
+                    <div className="cursor-pointer rounded-[3px] bg-[#222222] px-[7px] text-sm font-normal text-white">
+                      No Fly Zone
+                    </div>
+                  )}
+                  {!!tags[3] && (
+                    <div className="cursor-pointer rounded-[3px] bg-[#E04F64] px-[7px] text-sm font-normal text-white">
+                      Review Offer
+                    </div>
+                  )}
+                  {activeTab === PortfolioTabEnum.UNVERIFIED && (
+                    <div
+                      onClick={handleCancelClaim}
+                      className="flex h-8 w-28 cursor-pointer items-center justify-center rounded-[3px] bg-[#4285F4] px-2 text-sm font-normal text-white"
+                    >
+                      Cancel Claim
+                    </div>
+                  )}
+
+                  {(documentStatus === "SUBMITTED" || underReview) && (
+                    <div className="flex items-center justify-center gap-2">
+                      <div className="h-6 w-6">
+                        <ReviewVerificationIcon />
                       </div>
-                      <p className="text-sm font-normal text-[#E04F64]">Documents rejected</p>
+                      <p className="text-sm font-normal text-orange-500">Documents under review</p>
+                    </div>
+                  )}
+                  {documentStatus === "APPROVED" && !underReview && (
+                    <div className="flex items-center justify-center gap-2">
+                      <div className="h-6 w-6">
+                        <DocumentApprovedIcon />
+                      </div>
+                      <p className="text-sm font-normal text-[#1FD387]">Documents approved</p>
+                    </div>
+                  )}
+                  {(documentStatus === "REJECTED" || documentStatus === "RE_UPLOAD") && !underReview && (
+                    <div className="">
+                      <div className="flex items-center justify-end">
+                        <div className="mr-[10px] h-4 w-4">
+                          <DocumentRejectedIcon />
+                        </div>
+                        <p className="text-sm font-normal text-[#E04F64]">Documents rejected</p>
+                      </div>
+                      <div className="h-[14px] w-[7px]">
+                        <ChevronRightIcon />
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {
+                  <div>
+                    {documentStatus === "RE_UPLOAD" && !underReview && (
+                      <button
+                        onClick={handleButtonClick}
+                        className="font mt-4 flex items-center rounded-[3px] border-[1px] border-[#F79663] px-[7px] text-[12px] leading-[26px] text-[#F79663]"
+                      >
+                        <pre>Re-updload</pre>
+                      </button>
+                    )}
+                  </div>
+                }
+
+                {documentStatus === "NOT_SUBMITTED" && !underReview && airspace?.requestDocument && (
+                  <div className="mt-4 flex w-full items-center justify-between gap-12">
+                    <div onClick={handleButtonClick} className="rounded-md border border-orange-500 p-2">
+                      <p className="text-sm font-normal text-orange-500">Additional documents requested</p>
                     </div>
                     <div className="h-[14px] w-[7px]">
                       <ChevronRightIcon />
                     </div>
                   </div>
                 )}
+
+                {(documentStatus === "SUBMITTED" || underReview) && airspace?.requestDocument && (
+                  <UploadedDocuments requestDocument={airspace?.requestDocument} />
+                )}
+                {showPopup && !underReview && airspace?.requestDocument && (
+                  <AdditionalDocuments
+                    setUnderReview={setUnderReview}
+                    showPopup={showPopup}
+                    setUploadedDoc={setUploadedDoc}
+                    setShowSuccessToast={setShowSuccessToast}
+                    closePopup={closePopup}
+                    requestDocument={airspace?.requestDocument[airspace?.requestDocument?.length - 1]}
+                  />
+                )}
+
+                {showSuccessToast && <VerificationSuccessPopup />}
               </div>
-              {
-                <div>
-                  {documentStatus === "RE_UPLOAD" && !underReview && (
-                    <button
-                      onClick={handleButtonClick}
-                      className="font mt-4 flex items-center rounded-[3px] border-[1px] border-[#F79663] px-[7px] text-[12px] leading-[26px] text-[#F79663]"
-                    >
-                      <pre>Re-updload</pre>
-                    </button>
-                  )}
-                </div>
-              }
-
-              {documentStatus === "NOT_SUBMITTED" && !underReview && requestDocument && (
-                <div className="mt-4 flex w-full items-center justify-between gap-12">
-                  <div onClick={handleButtonClick} className="rounded-md border border-orange-500 p-2">
-                    <p className="text-sm font-normal text-orange-500">Additional documents requested</p>
-                  </div>
-                  <div className="h-[14px] w-[7px]">
-                    <ChevronRightIcon />
-                  </div>
-                </div>
-              )}
-
-              {(documentStatus === "SUBMITTED" || underReview) && requestDocument && (
-                <UploadedDocuments requestDocument={requestDocument} />
-              )}
-              {showPopup && !underReview && requestDocument && (
-                <AdditionalDocuments
-                  setUnderReview={setUnderReview}
-                  showPopup={showPopup}
-                  setUploadedDoc={setUploadedDoc}
-                  setShowSuccessToast={setShowSuccessToast}
-                  closePopup={closePopup}
-                  requestDocument={requestDocument[requestDocument?.length - 1]}
-                />
-              )}
-
-              {showSuccessToast && <VerificationSuccessPopup />}
             </div>
           </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
